43e877245c9750429453062d3e250c2957d45513
Dhruv Shrivastava <dhruvsri5@gmail.com>
2021-10-18 00:41:49 +0530

    bug fixes

--- kitabkuber/books/urls.py
+++ kitabkuber/books/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from books import views
 
 urlpatterns = [
-    path('latest-books/', views.LatestBooksList.as_view()),
+    path('latest-books/', views.LatestBookDetail.as_view()),
     path('books/search/', views.search),
     path('books/<slug:category_slug>/<slug:product_slug>/', views.BookDetail.as_view()),
     path('books/<slug:category_slug>/', views.CategoryDetail.as_view()),
--- kitabkuber/kitabkuber/urls.py
+++ kitabkuber/kitabkuber/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
-    path('api/v1/', include('book.urls')),
+    path('api/v1/', include('books.urls')),
     path('api/v1/', include('order.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
