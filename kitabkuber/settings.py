bb16ff86869e72da6ddef426761d88e732fe3953
Dhruv Shrivastava <dhruvsri5@gmail.com>
2021-11-23 13:14:51 +0530

    final commits

--- kitabkuber/settings.py
+++ kitabkuber/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'kitabkuber-django.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'kitabkuber-django.herokuapp.com', 'kitabkuber-vue.herokuapp.com']
 
 
 # Application definition
