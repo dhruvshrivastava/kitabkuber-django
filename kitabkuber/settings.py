43be119eb7cbf616119fe106d0fea51e7a07f3e8
Dhruv Shrivastava <dhruvsri5@gmail.com>
2021-11-25 23:15:23 +0530

    Update settings.py

--- kitabkuber/settings.py
+++ kitabkuber/settings.py
@@ -192,6 +192,10 @@ LOGGING = {
     }
 }
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = 'kitabkuber'
+AWS_S3_REGION_NAME = 'ap-south-1'
+
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
